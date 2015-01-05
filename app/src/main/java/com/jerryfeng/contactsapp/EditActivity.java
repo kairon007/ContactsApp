15578592d58d91152e0be742efb4612b69e9ef13
JFENG41 <jerry.feng41@gmail.com>
2015-01-05 16:04:34 -0500

    Implemented deleting & editing of contacts
    Also tweaked expandable list view

--- app/src/main/java/com/jerryfeng/contactsapp/EditActivity.java
+++ app/src/main/java/com/jerryfeng/contactsapp/EditActivity.java
@@ -8,14 +8,13 @@ import android.content.SharedPreferences;
 import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
 
-public class AddNewActivity extends ActionBarActivity {
+public class EditActivity extends ActionBarActivity {
 
     //Widgets
     ImageButton photo;
@@ -23,30 +22,34 @@ public class AddNewActivity extends ActionBarActivity {
 
     //Saving persistent data
     SharedPreferences sharedPref;
+    int listPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_new);
+        setContentView(R.layout.activity_edit);
 
         //Up button
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         //Instantiating components
-        fieldName = (EditText)findViewById(R.id.addNew_name);
-        fieldNumber = (EditText)findViewById(R.id.addNew_number);
-        fieldEmail = (EditText)findViewById(R.id.addNew_email);
-
+        fieldName = (EditText)findViewById(R.id.edit_name);
+        fieldNumber = (EditText)findViewById(R.id.edit_number);
+        fieldEmail = (EditText)findViewById(R.id.edit_email);
 
         //Receive intent
-        //Intent intent = getIntent();
-
+        Intent intent = getIntent();
+        listPosition = intent.getIntExtra(getString(R.string.extra_listposition), -1);
+        fieldName.setText(intent.getStringExtra(getString(R.string.extra_name)));
+        fieldNumber.setText(intent.getStringExtra(getString(R.string.extra_number)));
+        fieldEmail.setText(intent.getStringExtra(getString(R.string.extra_email)));
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_add_new, menu);
+        getMenuInflater().inflate(R.menu.menu_edit, menu);
         return true;
     }
 
@@ -56,7 +59,7 @@ public class AddNewActivity extends ActionBarActivity {
             case android.R.id.home:     //Up button
                 NavUtils.navigateUpFromSameTask(this);
                 return true;
-            case R.id.menuAddNew_save:
+            case R.id.menuEdit_save:
                 if (fieldName.getText().toString().length() < 1) {
                     AlertDialog.Builder alert = new AlertDialog.Builder(this);
                     alert.setTitle("Save contact");
@@ -72,18 +75,14 @@ public class AddNewActivity extends ActionBarActivity {
                 sharedPref = getSharedPreferences(getString(R.string.pref_contacts_key), Context.MODE_PRIVATE);
                 SharedPreferences.Editor editor = sharedPref.edit();
 
-                //update length of list
-                int contactCount = sharedPref.getInt(getString(R.string.pref_contactCount), -1) + 1;
-                editor.putInt(getString(R.string.pref_contactCount), contactCount);
-
-                //save contact with key numbered with the next available index
-                editor.putString(getString(R.string.pref_contactName) + (contactCount - 1),
+                //save changes made to contact info
+                editor.putString(getString(R.string.pref_contactName) + listPosition,
                         fieldName.getText().toString());
-                editor.putString(getString(R.string.pref_contactNumber) + (contactCount - 1),
+                editor.putString(getString(R.string.pref_contactNumber) + listPosition,
                         fieldNumber.getText().toString());
-                editor.putString(getString(R.string.pref_contactEmail) + (contactCount - 1),
+                editor.putString(getString(R.string.pref_contactEmail) + listPosition,
                         fieldEmail.getText().toString());
-                editor.putString(getString(R.string.pref_contactImage) + (contactCount - 1),
+                editor.putString(getString(R.string.pref_contactImage) + listPosition,
                         "");
                 editor.commit();
 
--- app/src/main/java/com/jerryfeng/contactsapp/MainActivity.java
+++ app/src/main/java/com/jerryfeng/contactsapp/MainActivity.java
@@ -1,41 +1,39 @@
 package com.jerryfeng.contactsapp;
 
 import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
-import android.provider.ContactsContract;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ExpandableListView;
 import android.widget.ImageButton;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener, ExpandableListView.OnGroupClickListener {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener, ExpandableListView.OnGroupExpandListener, ExpandableListView.OnGroupCollapseListener {
 
     //Widgets
     ImageView previewPhoto;
-    TextView previewName, previewNumber, previewEmail;
+    TextView previewName;
     ImageButton buttonCall, buttonText;
 
     ExpandableListView contactsListView;
     ExpandableListAdapter contactsAdapter;
     ArrayList<Contact> contactsList;
-
+    int prevSelectedGroup;
 
     //Saving activity states for change in screen config etc.
     static final String STATE_PREVIEWNAME = "previewName";
+
+    //Saving persistent data
+    SharedPreferences sharedPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,29 +43,19 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         //Instantiating components
 
         previewName = (TextView)findViewById(R.id.main_previewName);
-        previewNumber = (TextView)findViewById(R.id.main_previewNumber);
-        previewEmail = (TextView)findViewById(R.id.main_previewEmail);
-
+        /*
         buttonCall = (ImageButton)findViewById(R.id.main_buttonCall);
         buttonCall.setOnClickListener(this);
         buttonText = (ImageButton)findViewById(R.id.main_buttonText);
-        buttonText.setOnClickListener(this);
+        buttonText.setOnClickListener(this);*/
 
         contactsListView = (ExpandableListView)findViewById(R.id.main_contactsList);
-        initContactList();
-        contactsAdapter = new ExpandableListAdapter(this, contactsList);
-        contactsListView.setAdapter(contactsAdapter);
-        contactsListView.setOnGroupClickListener(this);
-    }
-
-    private void initContactList() {
         contactsList = new ArrayList<Contact>();
-        Contact sampleContact = new Contact();
-        sampleContact.photoSrc="";
-        sampleContact.name="John Smith";
-        sampleContact.number="519 000 0000";
-        sampleContact.email="johnsmith@gmail.com";
-        contactsList.add(sampleContact);
+        contactsListView.setOnGroupExpandListener(this);
+        contactsListView.setOnGroupCollapseListener(this);
+        prevSelectedGroup = -1;
+        //loadContactList();
+
     }
 
     @Override
@@ -77,15 +65,29 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
     @Override
     public void onRestoreInstanceState(Bundle savedInstanceState) {
+        //loadContactList();
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        saveContactList();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        //Called both when app is started & returning from a child activity
+        //Call here or call separately in onCreate() and onRestart()?
+        loadContactList();
     }
 
         @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
-
-
 
         return true;
     }
@@ -98,15 +100,60 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
                 intentAddNew.setAction(Intent.ACTION_VIEW);
                 startActivity(intentAddNew);
                 return true;
-            case R.id.menu_edit:
 
+            case R.id.menu_edit:
+                if (prevSelectedGroup < 0) {
+                    AlertDialog.Builder alertEdit = new AlertDialog.Builder(this);
+                    alertEdit.setTitle("Edit contact");
+                    alertEdit.setMessage("Please select a contact to edit");
+                    alertEdit.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {}
+                    });
+                    alertEdit.show();
+                }
+                else {
+                    Intent intentEdit = new Intent(this, EditActivity.class);
+                    intentEdit.setAction(Intent.ACTION_VIEW);
+                    Contact contact = contactsList.get(prevSelectedGroup);
+                    Bundle bundle = new Bundle();
+                    intentEdit.putExtra(getString(R.string.extra_listposition), prevSelectedGroup);
+                    intentEdit.putExtra(getString(R.string.extra_name), contact.name);
+                    intentEdit.putExtra(getString(R.string.extra_number), contact.number);
+                    intentEdit.putExtra(getString(R.string.extra_email), contact.email);
+                    intentEdit.putExtra(getString(R.string.extra_image), contact.imageSrc);
+                    startActivity(intentEdit);
+                }
                 return true;
+
             case R.id.menu_delete:
-                //if (contactsListView)
                 AlertDialog.Builder alertDelete = new AlertDialog.Builder(this);
                 alertDelete.setTitle("Delete contact");
-
+                if (prevSelectedGroup < 0) {
+                    alertDelete.setMessage("Please select a contact to delete");
+                    alertDelete.setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {}
+                    });
+                    alertDelete.show();
+                }
+                else {
+                    alertDelete.setMessage("Are you sure you want to delete this contact?");
+                    alertDelete.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            contactsList.remove(prevSelectedGroup);
+                            saveContactList();
+                        }
+                    });
+                    alertDelete.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {}
+                    });
+                    alertDelete.show();
+                }
                 return true;
+
             default:
                 return super.onOptionsItemSelected(item);
         }
@@ -115,28 +162,69 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.main_buttonCall:
-                break;
-            case R.id.main_buttonText:
-                break;
+
         }
     }
 
     @Override
-    public boolean onGroupClick(ExpandableListView parent, View v, int groupPosition, long id) {
-        //Log.d("ContactsApp", parent + "");
-
-        if (parent.isGroupExpanded(groupPosition)) {
-            previewName.setText(R.string.main_previewName);
-            previewNumber.setText(R.string.main_previewNumber);
-            previewEmail.setText(R.string.main_previewEmail);
+    public void onGroupExpand(int groupPosition) {
+        //Collapse all other groups except for currently selected one
+        if (groupPosition != prevSelectedGroup) {
+            contactsListView.collapseGroup(prevSelectedGroup);
         }
-        else {
-            previewName.setText(contactsList.get(groupPosition).name);
-            previewNumber.setText(contactsList.get(groupPosition).number);
-            previewEmail.setText(contactsList.get(groupPosition).email);
-        }
-
-        return false;
+        prevSelectedGroup = groupPosition;
     }
+
+    @Override
+    public void onGroupCollapse(int groupPosition) {
+        //Reset selected group
+        prevSelectedGroup = -1;
+    }
+
+    private void loadContactList() {
+        sharedPref = getSharedPreferences(getString(R.string.pref_contacts_key), Context.MODE_PRIVATE);
+
+        //get updated length of list
+        int listLength = sharedPref.getInt(getString(R.string.pref_contactCount), 0);
+        //get each contact from numbered pref keys
+        for (int i = 0; i < listLength; i++) {
+            Contact contact = new Contact();
+            contact.name = sharedPref.getString(getString(R.string.pref_contactName) + i, "");
+            contact.number = sharedPref.getString(getString(R.string.pref_contactNumber) + i, "");
+            contact.email = sharedPref.getString(getString(R.string.pref_contactEmail) + i, "");
+            contact.imageSrc = sharedPref.getString(getString(R.string.pref_contactImage) + i, "");
+
+            if (i >= contactsList.size()) {     //if index is beyond current list length,
+                contactsList.add(contact);      //add as new entry
+            } else {                            //if index is within current list length,
+                contactsList.set(i, contact);   //replace existing entry
+            }
+        }
+        //Update list adapter
+        contactsAdapter = new ExpandableListAdapter(this, contactsList);
+        contactsListView.setAdapter(contactsAdapter);
+        contactsAdapter.notifyDataSetChanged();
+
+    }
+
+    private void saveContactList() {
+        sharedPref = getSharedPreferences(getString(R.string.pref_contacts_key), Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        //save length of list
+        editor.putInt(getString(R.string.pref_contactCount), contactsList.size());
+        //save each contact with numbered pref keys
+        for (int i = 0; i < contactsList.size(); i++) {
+            editor.putString(getString(R.string.pref_contactName) + i, contactsList.get(i).name);
+            editor.putString(getString(R.string.pref_contactNumber) + i, contactsList.get(i).number);
+            editor.putString(getString(R.string.pref_contactEmail) + i, contactsList.get(i).email);
+            editor.putString(getString(R.string.pref_contactImage) + i, contactsList.get(i).imageSrc);
+        }
+        editor.commit();
+        //Update list adapter
+        contactsAdapter = new ExpandableListAdapter(this, contactsList);
+        contactsListView.setAdapter(contactsAdapter);
+        contactsAdapter.notifyDataSetChanged();
+    }
+
 }
